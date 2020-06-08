62f2ebc71e861de12d4add80e8ad3a153707343e
axiang <wyxworkmail@163.com>
2020-06-09 00:21:52 +0800

    feat: docker容器化支持

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM nginx
+LABEL user="axiangcoding"
 COPY dist/spa/ /usr/share/nginx/html/
 COPY nginx/default.conf /etc/nginx/conf.d/default.conf
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,8 @@
-docker build -t codingfirstwebcli .
+# 暂不支持自动化部署，等待进一步更新
+docker pull nginx
+docker build -t coding_first_web_cli_new .
+docker run -dp 9999:80 -d --name coding_first_web_cli_new coding_first_web_cli_new
+
+
+
+
