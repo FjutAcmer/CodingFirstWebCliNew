411528b8fd97ebc484cee4972e1552feba315223
axiang <wyxworkmail@163.com>
2020-06-09 16:14:08 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 # 暂不支持自动化部署，等待进一步更新
+npm install
+npm run build
 docker pull nginx
 docker build -t coding_first_web_cli_new .
 docker run -dp 9999:80 -d --name coding_first_web_cli_new coding_first_web_cli_new
