46ddde6f1a5098b8841ce7ab56ea1e0100be8dc4
axiangcoding <wyxworkmail@163.com>
2020-06-09 16:55:28 +0800

    update deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ npm run build
 sudo docker pull nginx
 sudo docker stop coding_first_web_cli_new
 sudo docker rm coding_first_web_cli_new
+sudo docker rmi coding_first_web_cli_new
 sudo docker build -t coding_first_web_cli_new .
 sudo docker run -dp 9999:80 -d --name coding_first_web_cli_new coding_first_web_cli_new
 
