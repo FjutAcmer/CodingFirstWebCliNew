05e56014474e4f10019ac68fc50b130740b51773
axiang <wyxworkmail@163.com>
2020-06-09 16:18:55 +0800

    test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 npm install
 npm run build
 docker pull nginx
+docker stop coding_first_web_cli_new
+docker rm coding_first_web_cli_new
 docker build -t coding_first_web_cli_new .
 docker run -dp 9999:80 -d --name coding_first_web_cli_new coding_first_web_cli_new
 
