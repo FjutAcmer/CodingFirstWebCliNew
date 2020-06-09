e3036fdc910aff68f21087b49f1cbb1ec50b8d00
axiangcoding <wyxworkmail@163.com>
2020-06-09 16:37:06 +0800

    update deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 # 暂不支持自动化部署，等待进一步更新
 npm install
 npm run build
-docker pull nginx
-docker stop coding_first_web_cli_new
-docker rm coding_first_web_cli_new
-docker build -t coding_first_web_cli_new .
-docker run -dp 9999:80 -d --name coding_first_web_cli_new coding_first_web_cli_new
+sudo docker pull nginx
+sudo docker stop coding_first_web_cli_new
+sudo docker rm coding_first_web_cli_new
+sudo docker build -t coding_first_web_cli_new .
+sudo docker run -dp 9999:80 -d --name coding_first_web_cli_new coding_first_web_cli_new
 
 
 
